844aa88ee0bf8004bf232dbb919d1adb1b3600e8
Alex Bishop <alexwbishop@proton.me>
2024-06-25 15:44:39 -0500

    get

--- ollama-prompt-tester.py
+++ ollama-prompt-tester.py
@@ -47,7 +47,7 @@ def load_prompts(filename):
 def load_all_prompts(filename):
     with open(filename, 'r') as f:
         data = json.load(f)
-    return data['categories', {}]
+    return data.get('categories', {})
 
 def save_prompts(filename, prompts):
     with open(filename, 'w') as f:
